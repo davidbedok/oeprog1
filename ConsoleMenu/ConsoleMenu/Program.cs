aac969aa3fff63401e79cfa3bf52e86a0afade8c
David Bedok <qwaevisz@gmail.com>
2012-05-28 08:58:20 +0200

    ADD: ConsoleMenu

---
+++ ConsoleMenu/ConsoleMenu/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleMenu
+{
+    public class Program
+    {
+
+        private static readonly int WINDOW_WIDTH = 100;
+        private static readonly int WINDOW_HEIGHT = 40;
+
+        private const string MENUITEM_TEST = "Test menupoint";
+        private const string MENUITEM_DUMMY = "Dummy menupoint";
+        private const string MENUITEM_CHILDMENU = "Child menu";
+        private const string MENUITEM_BACK = "Back";
+        private const string MENUITEM_EXIT = "Exit";
+
+        private static void writeMessage( int x, int y, string label)
+        {
+            System.Console.BackgroundColor = ConsoleColor.Gray;
+            System.Console.ForegroundColor = ConsoleColor.Black;
+            System.Console.SetCursorPosition(x, y);
+            System.Console.Write(label.PadRight(30));
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.SetWindowSize(Program.WINDOW_WIDTH, Program.WINDOW_HEIGHT);
+            Console.Title = "Simple Console Menu Demo";
+            System.Console.BackgroundColor = ConsoleColor.Red;
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.SetCursorPosition(0,0);
+            System.Console.Write(" Simple Console Menu Demo Application @ UNI-OBUDA 2011".PadRight(Program.WINDOW_WIDTH));
+            System.Console.BackgroundColor = ConsoleColor.Gray;
+
+            SimpleMenu menu = new SimpleMenu(5, 5, 5, ConsoleColor.Blue);
+            menu.MenuItemColor = ConsoleColor.Black;
+            menu.ActiveMenuBgr = ConsoleColor.Yellow;
+            menu.addMenuItem(Program.MENUITEM_TEST);
+            menu.addMenuItem(Program.MENUITEM_DUMMY);
+            menu.addMenuItem(Program.MENUITEM_CHILDMENU);
+            menu.addMenuItem(Program.MENUITEM_EXIT);
+
+            SimpleMenu childMenu = new SimpleMenu(9, 14, 5, ConsoleColor.Gray);
+            childMenu.MenuItemColor = ConsoleColor.Black;
+            childMenu.ActiveMenuBgr = ConsoleColor.Yellow;
+            childMenu.addMenuItem(Program.MENUITEM_TEST);
+            childMenu.addMenuItem(Program.MENUITEM_DUMMY);
+            childMenu.addMenuItem(Program.MENUITEM_BACK);
+
+            int menuIndex;
+            do {
+                menuIndex = menu.process();
+                switch (menu.getActiveMenuLabel())
+                {
+                    case Program.MENUITEM_TEST:
+                        Program.writeMessage(1,1,Program.MENUITEM_TEST);
+                        break;
+                    case Program.MENUITEM_DUMMY:
+                        Program.writeMessage(1, 1, Program.MENUITEM_DUMMY);
+                        break;
+                    case Program.MENUITEM_CHILDMENU:
+
+                        int childMenuIndex;
+                        do {
+                            childMenuIndex = childMenu.process();
+                            switch (childMenu.getActiveMenuLabel())
+                            {
+                                case Program.MENUITEM_TEST:
+                                    Program.writeMessage(1, 1, Program.MENUITEM_TEST);
+                                    break;
+                                case Program.MENUITEM_DUMMY:
+                                    Program.writeMessage(1, 1, Program.MENUITEM_DUMMY);
+                                    break;
+                            }
+                        } while (!childMenu.getActiveMenuLabel().Equals(Program.MENUITEM_BACK));
+                        break;
+                }
+
+            } while (!menu.getActiveMenuLabel().Equals(Program.MENUITEM_EXIT));
+
+        }
+    }
+}
---
+++ ConsoleMenu/ConsoleMenu/SimpleMenu.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleMenu
+{
+    public class SimpleMenu
+    {
+
+        private int top;
+        private int left;
+        private string[] menuItems;
+        private int menuIndex;
+        private int activeMenuIndex;
+        private ConsoleColor bgrColor;
+        private ConsoleColor menuItemColor;
+        private ConsoleColor activeMenuBgr;
+
+        private int Top
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.top = value;
+                }
+            }
+        }
+
+        private int Left
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.left = value;
+                }
+            }
+        }
+
+        public ConsoleColor MenuItemColor
+        {
+            get { return this.menuItemColor; }
+            set { this.menuItemColor = value; }
+        }
+
+        public ConsoleColor ActiveMenuBgr
+        {
+            get { return this.activeMenuBgr; }
+            set { this.activeMenuBgr = value; }
+        }
+
+        public SimpleMenu(int top, int left, int maxMenuItem, ConsoleColor bgrColor)
+        {
+            if (maxMenuItem < 1)
+            {
+                maxMenuItem = 1;
+            }
+            this.Top = top;
+            this.Left = left;
+            this.menuItems = new string[maxMenuItem];
+            this.menuIndex = 0;
+            this.bgrColor = bgrColor;
+            this.activeMenuIndex = 0;
+            this.menuItemColor = ConsoleColor.White;
+            this.activeMenuBgr = ConsoleColor.Yellow;
+        }
+
+        public void addMenuItem(string label)
+        {
+            if (this.menuIndex < this.menuItems.Length)
+            {
+                this.menuItems[this.menuIndex++] = label;
+            }
+        }
+
+        public void down()
+        {
+            if (this.activeMenuIndex < this.menuIndex - 1)
+            {
+                this.activeMenuIndex++;
+            }
+            else
+            {
+                this.activeMenuIndex = 0;
+            }
+        }
+
+        public void up()
+        {
+            if (this.activeMenuIndex > 0)
+            {
+                this.activeMenuIndex--;
+            }
+            else
+            {
+                this.activeMenuIndex = this.menuIndex - 1;
+            }
+        }
+
+        private int getMaxMenuItemLength()
+        {
+            int ret = 0;
+            if ( this.menuIndex > 0 ) {
+                ret = this.menuItems[0].Length;
+                for (int i = 1; i < this.menuIndex; i++)
+                {
+                    if (ret < this.menuItems[i].Length)
+                    {
+                        ret = this.menuItems[i].Length;
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private void drawMenuTop(int maxLength)
+        {
+            System.Console.SetCursorPosition(this.left, this.top);
+            System.Console.WriteLine("╔" + "".PadLeft(maxLength, '═') + "╗");
+        }
+
+        private void drawMenuMiddle(int maxLength, int row)
+        {
+            System.Console.SetCursorPosition(this.left, this.top + row);
+            System.Console.WriteLine("╠" + "".PadLeft(maxLength, '═') + "╣");
+        }
+
+        private void drawMenuBottom(int maxLength, int row)
+        {
+            System.Console.SetCursorPosition(this.left, this.top + row);
+            System.Console.WriteLine("╚" + "".PadLeft(maxLength, '═') + "╝");
+        }
+
+        private void setMenuItemBgr( int i ){
+            if (i == this.activeMenuIndex)
+            {
+                System.Console.BackgroundColor = this.activeMenuBgr;
+            }
+            else
+            {
+                System.Console.BackgroundColor = this.bgrColor;
+            }
+        }
+
+        public void draw()
+        {
+            System.Console.ForegroundColor = this.menuItemColor;
+            System.Console.BackgroundColor = this.bgrColor;
+            int maxLength = this.getMaxMenuItemLength();
+
+            this.drawMenuTop(maxLength);
+
+            int row = 0;
+            for (int i = 0; i < this.menuIndex; i++)
+            {
+                System.Console.SetCursorPosition(this.left, this.top + ++row);
+                System.Console.BackgroundColor = this.bgrColor;
+                System.Console.Write("│");
+                this.setMenuItemBgr(i);
+                System.Console.Write(this.menuItems[i].PadRight(maxLength, ' '));
+                System.Console.BackgroundColor = this.bgrColor;
+                System.Console.Write("│");
+
+                if (i != this.menuIndex - 1)
+                {
+                    this.drawMenuMiddle(maxLength, ++row);
+                }
+            }
+            this.drawMenuBottom(maxLength, ++row);
+        }
+
+        public int process()
+        {
+            ConsoleKey cKey;
+            do
+            {
+                this.draw();
+                cKey = Console.ReadKey(true).Key;
+                switch (cKey)
+                {
+                    case ConsoleKey.UpArrow:
+                        this.up();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        this.down();
+                        break;
+                }
+            } while ((cKey != ConsoleKey.Escape) && (cKey != ConsoleKey.Enter));
+            return this.activeMenuIndex;
+        }
+
+        private string getMenuLabelByIndex(int index){
+            string ret = "";
+            if (index < this.menuIndex)
+            {
+                ret = this.menuItems[index];
+            }
+            return ret;
+        }
+
+        public string getActiveMenuLabel()
+        {
+            return this.getMenuLabelByIndex(this.activeMenuIndex);
+        }
+
+
+    }
+}
