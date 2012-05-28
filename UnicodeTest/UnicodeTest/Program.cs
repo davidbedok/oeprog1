95bbf55b01c68d04a2ab6fd27d9aacddac0ece69
David Bedok <qwaevisz@gmail.com>
2012-05-28 09:04:38 +0200

    ADD: UnicodeTest

---
+++ UnicodeTest/UnicodeTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnicodeTest
+{
+    public class Program
+    {
+
+        private readonly String value;
+
+        private void testUnicode(bool \u0066)
+        {
+            char c = '\u0066';
+            if (\u0066)
+            {
+                System.Console.WriteLine(c.ToString());
+            }
+        }
+
+        private void test(bool f)
+        {
+            char c = 'f';
+            if (f)
+            {
+                System.Console.WriteLine(c.ToString());
+            }
+        }     
+
+	    public Program() {
+            this.test(true);
+            this.testUnicode(true);
+	    }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
