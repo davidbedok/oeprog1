940e4dafb0eb339f1cc0c81e3117ca5b47b4619a
David Bedok <qwaevisz@gmail.com>
2012-05-28 09:00:49 +0200

    ADD: Equilibrium

---
+++ Equilibrium/Equilibrium/Equilibrium.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Equilibrium
+{
+    public class Equilibrium
+    {
+        public static int equilibrium(int[] arr)
+        {
+            int ret = -1;
+            int n = arr.Length;
+            int leftsum = 0;
+            int rightsum = 0;
+            int sum = 0;
+            int i;
+            for (i = 0; i < n; ++i)
+            {
+                sum += arr[i];
+            }
+            i = 0;
+            while ((i < n) && (ret == -1))
+            {
+                rightsum = sum - leftsum - arr[i];
+                if (leftsum == rightsum)
+                {
+                    ret = i;
+                }
+                else
+                {
+                    leftsum += arr[i];
+                    i++;
+                }
+            }
+            return ret;
+        }
+
+        public static int equilibriumExpert(int[] arr)
+        {
+            int i;
+            int sum = 0;
+            for (i = 0; i < arr.Length; ++i)
+            {
+                sum += arr[i];
+            }
+            while (i-- > 0 && arr[i] != sum)
+            {
+                sum -= 2 * arr[i];
+            }
+            return i;
+        }
+
+    }
+}
---
+++ Equilibrium/Equilibrium/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Equilibrium
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine(Equilibrium.equilibrium(new int[] { -7, 1, 5, 2, -4, 3, 0 }));
+        }
+    }
+}
