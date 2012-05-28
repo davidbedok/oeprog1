4134e789618bd674a416efa102fe3411a1e3552a
David Bedok <qwaevisz@gmail.com>
2012-05-28 08:52:04 +0200

    ADD: ConsoleGraphics

---
+++ ConsoleGraphics/ConsoleGraphics/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleGraphics
+{
+    public class Program
+    {
+        private static readonly int WINDOW_WIDTH = 70;
+        private static readonly int WINDOW_HEIGHT = 40;
+
+        private static ConsoleColor getRandomConsoleColor(Random rand)
+        {
+            // inclusive lower, exclusive upper bound
+            byte num = (byte)rand.Next(0, 16);
+            return (ConsoleColor)num;
+        }
+
+        private static char getRandomShape(Random rand)
+        {
+            int num = rand.Next(40, 90);
+            return (char)num;
+        }
+
+        private static MoveType getRandomMove(Random rand)
+        {
+            byte num = (byte)rand.Next(0, 4);
+            return (MoveType)num;
+        }
+
+        private static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            
+            Console.SetWindowSize(Program.WINDOW_WIDTH, Program.WINDOW_HEIGHT);
+            Console.Title = "Console Graphics Demo";
+
+            Shape iShape = new Shape(Console.WindowWidth, Console.WindowHeight, Console.BackgroundColor, ConsoleColor.Red, '@');
+            iShape.draw();
+            ConsoleKey cKey;
+            do {
+                cKey = Console.ReadKey(true).Key;
+                switch (cKey)
+                {
+                    case ConsoleKey.LeftArrow:
+                        iShape.left();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        iShape.right();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        iShape.up();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        iShape.down();
+                        break;
+                    default:
+                        // Console.WriteLine(cKey);
+                        break;
+                }
+            } while (cKey != ConsoleKey.Escape);
+
+            Shape[] shapes = new Shape[10];
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                shapes[i] = new Shape(Console.WindowWidth, Console.WindowHeight, Console.BackgroundColor, Program.getRandomConsoleColor(rand), Program.getRandomShape(rand));
+            }
+
+            bool exitCycle = false;
+            do {
+                
+                for (int i = 0; i < shapes.Length; i++)
+                {
+                    shapes[i].move(Program.getRandomMove(rand));
+                }
+
+                if (Console.KeyAvailable)
+                {
+                    exitCycle = (Console.ReadKey(true).Key == ConsoleKey.Escape);
+                }
+            } while (!exitCycle);
+            
+        }
+    }
+}
---
+++ ConsoleGraphics/ConsoleGraphics/Shape.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleGraphics
+{
+    public class Shape
+    {
+        private readonly int width;
+        private readonly int height;
+        private readonly ConsoleColor bgrColor;
+
+        private int positionX;
+        private int positionY;
+        private ConsoleColor color;
+        private char shape;
+
+        // private
+        public int PositionX
+        {
+            get
+            {
+                return this.positionX;
+            }
+            set
+            {
+                if ((value >= 0) && (value < this.width))
+                {
+                    this.positionX = value;
+                }
+            }
+        }
+
+        public int PositionY
+        {
+            get
+            {
+                return this.positionY;
+            }
+            set
+            {
+                if ((value >= 0) && (value < this.height))
+                {
+                    this.positionY = value;
+                }
+            }
+        }
+
+        public Shape(int width, int height, ConsoleColor bgrColor, ConsoleColor color, char shape)
+        {
+            this.width = width;
+            this.height = height;
+            this.bgrColor = bgrColor;
+            this.color = color;
+            this.PositionX = width / 2; // this.positionX = width / 2;
+            this.PositionY = height / 2;
+            this.shape = shape;
+        }
+   
+        public void draw()
+        {
+            this.draw(this.color);
+        }
+
+        public void erase()
+        {
+            this.draw(this.bgrColor);
+        }
+
+        private void draw( ConsoleColor color )
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.SetCursorPosition(this.positionX, this.positionY);
+            System.Console.Write(this.shape);
+        }
+
+        public void left()
+        {
+            this.move(MoveType.LEFT);
+        }
+
+        public void right()
+        {
+            this.move(MoveType.RIGHT);
+        }
+
+        public void up()
+        {
+            this.move(MoveType.UP);
+        }
+
+        public void down()
+        {
+            this.move(MoveType.DOWN);
+        }
+
+        public void move(MoveType moveType)
+        {
+            this.erase();
+            switch (moveType)
+            {
+                case MoveType.LEFT:
+                    this.PositionX = this.positionX - 1;
+                    break;
+                case MoveType.RIGHT:
+                    this.PositionX = this.positionX + 1;
+                    break;
+                case MoveType.UP:
+                    this.PositionY = this.positionY - 1;
+                    break;
+                case MoveType.DOWN:
+                    this.PositionY = this.positionY + 1;
+                    break;
+            }
+            this.draw();
+        }
+
+        public override string ToString()
+        {
+            return "Shape ["+this.positionX+":"+this.positionY+"]";
+        }
+
+    }
+}
